f35ff0cdc9f568063cc0b2625221408df50126d9
f3prosystem <francivaldo.alves@gmail.com>
2018-05-21 14:33:05 -0300

    first commit

---
+++ src/main/java/f3prosystem/com/projeto1/entity/Role.java
@@ -0,0 +1,29 @@
+package f3prosystem.com.projeto1.entity;
+
+import javax.persistence.*;
+
+
+@Entity (name="perfil")
+public class Role {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
